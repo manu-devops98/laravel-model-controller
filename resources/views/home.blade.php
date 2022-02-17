9e44b41707dc2e4d41252103877c02259c1396af
manu-devops98 <manuelcappello7@gmail.com>
2022-02-17 16:07:14 +0100

    add scss layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,14 @@
 @endsection
 
 @section('content')
-    @dd($movie);
+<div class="container">
+    @foreach ($movie as $singleMovie)
+    <div class="card">
+        <h1>Titolo: <span>{{$singleMovie->title}}</span></h1>
+        <h3>Titolo Originale: <span>{{$singleMovie->original_title}}</span></h3>
+        <h4>Nazionalità: <span>{{ $singleMovie->nationality}}</span></h4>
+        <h5>Vote: <span>{{ $singleMovie->vote}}</span></h5>
+    </div>
+    @endforeach
+</div>
 @endsection
